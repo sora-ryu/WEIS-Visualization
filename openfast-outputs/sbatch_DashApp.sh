3f958c2ab662d108b7e57bc89cadb5dc7930643c
Mayank Chetan <mayankchetan@gmail.com>
2023-12-05 21:14:48 -0700

    Simple working app + eagle scripts

---
+++ openfast-outputs/sbatch_DashApp.sh
@@ -0,0 +1,20 @@
+## Modify walltime and account at minimum
+#SBATCH --time=00:10:00
+#SBATCH --account=bar
+
+#SBATCH --nodes=1
+#SBATCH --tasks-per-node=1
+#SBATCH --cpus-per-task=36
+
+module purge
+module load conda
+
+
+source activate /projects/bar/mchetan/weis-p2/env/weis-viz
+port=8050
+
+echo "run the following command on your machine"
+echo ""
+echo "ssh -L $port:$HOSTNAME:$port $SLURM_SUBMIT_HOST.hpc.nrel.gov"
+
+python simpleDashApp.py
---
+++ openfast-outputs/simpleDashApp.py
@@ -0,0 +1,46 @@
+from dash import Dash, Input, Output, callback, dcc, html
+import copy
+import plotly.express as px
+import pandas as pd
+
+df = pd.read_csv('of-output/NREL5MW_OC3_spar_0.out', skiprows=[0,1,2,3,4,5,7], delim_whitespace=True)
+
+
+app = Dash(__name__)
+
+app.layout = html.Div(
+    [
+        html.H5("Signal-y"),
+        dcc.Dropdown(
+            id="signaly", options=sorted(df.keys())
+        ),
+        html.H5("Signal-x"),
+        dcc.Dropdown(
+            id="signalx", options=sorted(df.keys())
+        ),
+        html.Br(),
+        dcc.Graph(id="line"),
+    ]
+)
+
+
+@callback(
+    Output("line", "figure"),
+    Input("signalx", "value"),
+    Input("signaly", "value"),
+)
+def line_chart(signalx,signaly):
+
+    fig = px.line(
+        x=df[signalx],
+        y=df[signaly],
+        template="simple_white",
+        labels={"x": signalx, "y": signaly},
+    )
+
+    return fig
+
+
+if __name__ == "__main__":
+    app.run(debug=True, host="localhost")
+ 
