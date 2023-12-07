aacf78576dd2e48dbaa85aac31dc108b84971a2a
Sora Ryu <Sora.Ryu@nrel.gov>
2023-12-07 16:55:49 -0700

    add plot options

--- openfast-outputs/simpleDashApp.py
+++ openfast-outputs/simpleDashApp.py
@@ -2,28 +2,41 @@
 from dash import Dash, Input, Output, callback, dcc, html, dash_table
 import copy
 import plotly.express as px
+import plotly.graph_objects as go
+import plotly.tools as tls
+from plotly.subplots import make_subplots
+import dash_mantine_components as dmc
 import pandas as pd
 
 
 # Incorporate Data
-df = pd.read_csv('of-output/NREL5MW_OC3_spar_0.out', skiprows=[0,1,2,3,4,5,7], delim_whitespace=True)
+data_path = 'of-output/'
+data_file = 'NREL5MW_OC3_spar_0.out'
+df = pd.read_csv(data_path + data_file, skiprows=[0,1,2,3,4,5,7], delim_whitespace=True)
 
 # Initialize the app - Internally starts the Flask Server
-app = Dash(__name__)
+# Incorporate a Dash Mantine theme
+external_stylesheets = [dmc.theme.DEFAULT_COLORS]
+app = Dash(__name__, external_stylesheets = external_stylesheets)
 
 # App Layout
 app.layout = html.Div(
     [
-        html.Div(children='NREL5MW_OC3_spar_0 data'),
+        html.Div(data_file.split('.')[-2]),
         dash_table.DataTable(data=df.to_dict('records'), page_size=10),
 
         html.H5("Signal-y"),
         dcc.Dropdown(
-            id="signaly", options=sorted(df.keys())
+            id="signaly", options=sorted(df.keys()), multi=True
         ),
         html.H5("Signal-x"),
         dcc.Dropdown(
             id="signalx", options=sorted(df.keys())
+        ),
+        html.Br(),
+        html.H5("Plot Options"),
+        dcc.RadioItems(
+            options=['single plot', 'multiple plot'], value='single plot', inline=True, id='plotOption'
         ),
         html.Br(),
         dcc.Graph(id="line"),
@@ -34,18 +47,40 @@ app.layout = html.Div(
 @callback(
     Output(component_id="line", component_property="figure"),
     Input(component_id="signalx", component_property="value"),
-    Input(component_id="signaly", component_property="value"),
+    [Input(component_id="signaly", component_property="value")],
+    Input(component_id="plotOption", component_property="value")
 )
 
-def line_chart(signalx,signaly):
+def draw_graphs(signalx, signaly, plotOption):
 
-    fig = px.line(
-        x=df[signalx],
-        y=df[signaly],
-        template="simple_white",
-        labels={"x": signalx, "y": signaly},
-    )
+    # fig = tls.make_subplots(rows=1, cols=1, shared_xaxes=True, verical_spacing=0.009, horizontal_spacing=0.009)
+    # fig = go.Figure()
 
+    if plotOption == 'single plot':
+        fig = make_subplots(rows = 1, cols = 1)
+
+        for col_idx, label in enumerate(signaly):
+            fig.append_trace(go.Scatter(
+                x = df[signalx],
+                y = df[label],
+                mode = 'lines',
+                name = label),
+                row = 1,
+                col = 1)
+
+    elif plotOption == 'multiple plot':
+        fig = make_subplots(rows = 1, cols = len(signaly))
+
+        for col_idx, label in enumerate(signaly):
+            fig.append_trace(go.Scatter(
+                x = df[signalx],
+                y = df[label],
+                mode = 'lines',
+                name = label),
+                row = 1,
+                col = col_idx + 1)
+
+    
     return fig
 
 
