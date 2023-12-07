48705660c7023a2f276ad0b85a9d08085466ce53
Sora Ryu <Sora.Ryu@nrel.gov>
2023-12-07 14:34:33 -0700

    add comments/table

--- openfast-outputs/simpleDashApp.py
+++ openfast-outputs/simpleDashApp.py
@@ -1,15 +1,22 @@
-from dash import Dash, Input, Output, callback, dcc, html
+# Import Packages
+from dash import Dash, Input, Output, callback, dcc, html, dash_table
 import copy
 import plotly.express as px
 import pandas as pd
 
+
+# Incorporate Data
 df = pd.read_csv('of-output/NREL5MW_OC3_spar_0.out', skiprows=[0,1,2,3,4,5,7], delim_whitespace=True)
 
-
+# Initialize the app - Internally starts the Flask Server
 app = Dash(__name__)
 
+# App Layout
 app.layout = html.Div(
     [
+        html.Div(children='NREL5MW_OC3_spar_0 data'),
+        dash_table.DataTable(data=df.to_dict('records'), page_size=10),
+
         html.H5("Signal-y"),
         dcc.Dropdown(
             id="signaly", options=sorted(df.keys())
@@ -23,12 +30,13 @@ app.layout = html.Div(
     ]
 )
 
-
+# Add controls to build the interaction
 @callback(
-    Output("line", "figure"),
-    Input("signalx", "value"),
-    Input("signaly", "value"),
+    Output(component_id="line", component_property="figure"),
+    Input(component_id="signalx", component_property="value"),
+    Input(component_id="signaly", component_property="value"),
 )
+
 def line_chart(signalx,signaly):
 
     fig = px.line(
@@ -41,6 +49,7 @@ def line_chart(signalx,signaly):
     return fig
 
 
+# Run the app
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0",port="3030")
  
