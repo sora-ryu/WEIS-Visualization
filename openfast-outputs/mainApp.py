81afc865e11b17cfd4c7dd032de41c66c0862010
Sora Ryu <Sora.Ryu@nrel.gov>
2024-01-08 23:32:00 -0700

    Multi-page app structure with sharing data

--- openfast-outputs/mainApp.py
+++ openfast-outputs/mainApp.py
@@ -11,99 +11,52 @@ import plotly.graph_objects as go
 import plotly.tools as tls
 from plotly.subplots import make_subplots
 import dash_mantine_components as dmc
+import dash_bootstrap_components as dbc
 import pandas as pd
-
-# Connect to your app pages
-from pages import visualize_openfast
+import logging
 
 
 # Initialize the app - Internally starts the Flask Server
 # Incorporate a Dash Mantine theme
-external_stylesheets = [dmc.theme.DEFAULT_COLORS]
-app = Dash(__name__, external_stylesheets = external_stylesheets)
+# external_stylesheets = [dmc.theme.DEFAULT_COLORS]
+external_stylesheets = [dbc.themes.BOOTSTRAP]
+APP_TITLE = "WEIS Visualization APP"
+app = Dash(__name__, external_stylesheets = external_stylesheets, suppress_callback_exceptions=True, title=APP_TITLE, use_pages=True)
 
-app.layout = html.Div([
-    # dcc.Graph(id='MyGraph', animate=True),
-    dcc.Location(id='url', refresh=False),
-    dcc.Store(id='stored-data', storage_type='session'),
-    # Upload data
-    dcc.Upload(
-        id='upload-data', children=html.Div([
-            'Drag and Drop or ',
-            html.A('Select Files')
-        ]),
-        style={
-            'width': '100%',
-            'height': '60px',
-            'lineHeight': '60px',
-            'borderWidth': '1px',
-            'borderStyle': 'dashed',
-            'borderRadius': '5px',
-            'textAlign': 'center',
-            'margin': '10px'
-        },
-        # multiple=True         # Allow multiple files to be uploaded
-    ),
-    html.Div(id='output-data-upload')
-])
+# Build Simple Navigation Bar
+navbar = dbc.NavbarSimple(
+    children = [
+        dbc.NavItem(dbc.NavLink("Home", href='/')),
+        dbc.NavItem(dbc.NavLink("Data Upload", href='/data_upload')),
+        dbc.NavItem(dbc.NavLink("OpenFAST", href='/open_fast')),
+        dbc.NavItem(dbc.NavLink("Optimize", href='/optimize')),
+        dbc.NavItem(dbc.NavLink("3D Visualization", href='/3d_vis'))
+    ],
+    brand = APP_TITLE,
+    color = "dark",
+    dark = True,
+    className = "menu-bar"
+)
 
-def parse_contents(contents, filename, date):
-    content_type, content_string = contents.split(',')
-    decoded = base64.b64decode(content_string)
-    try:
-        df = pd.read_csv(io.StringIO(decoded.decode('utf-8')), skiprows=[0,1,2,3,4,5,7], delim_whitespace=True)
-    except Exception as e:
-        print(e)
-        return html.Div([
-            'There is some error on processing this file..'
-        ])
-    
-    return html.Div([
-        html.H5(f'File name: {filename}'),
-        html.H6(f'Date: {datetime.datetime.fromtimestamp(date)}'),
+# Wrap app with loading component
+app.layout = dcc.Loading(
+    id = 'loading_page_content',
+    children = [
+        html.Div(
+            [
+                dcc.Store(id='store', data={}),
+                navbar,
+                dash.page_container
+            ]
+        )
+    ],
+    color = 'primary',
+    fullscreen = True
+)
 
-        dash_table.DataTable(
-            data=df.to_dict('records'),
-            columns=[{'name': i, 'id': i} for i in df.columns],
-            page_size=10
-        ),
-        dcc.Store(id='stored-data', data=df.to_dict('records')),
 
-        html.Hr(),      # Horizontal line
-
-        html.Button('Visualize with plots', id='submit', n_clicks=0),       # refresh: controls whether or not the page will refresh when the link is clicked
-        html.Div(id='page-content', children=[])
-        # For debugging, display the raw contents provided by the web browser
-        # html.Div('Raw Content'),
-        # html.Pre(contents[0:200] + '...', style={
-        #     'whiteSpace': 'pre-wrap',
-        #     'wordBreak': 'break-all'
-        # })
-
-    ])
-
-@callback(Output('output-data-upload', 'children'),
-          Input('upload-data', 'contents'),
-          State('upload-data', 'filename'),
-          State('upload-data', 'last_modified'))
-def show_data_contents(contents, names, dates):
-    if contents is not None:
-        # 1) Single File version
-        children = parse_contents(contents, names, dates)
-
-        # 2) Multiple files version
-        # children = [
-        #     parse_contents(c, n, d) for c, n, d in zip(list_of_contents, list_of_names, list_of_dates)
-        # ]
-
-        return children
-
-@callback(Output('page-content', 'children'),
-          Input('submit', 'n_clicks'))
-def display_page(n_clicks):
-    if n_clicks > 0:
-        return visualize_openfast.layout
 
 # Run the app
 if __name__ == "__main__":
+    logging.basicConfig(level=logging.DEBUG)        # For debugging
     app.run(debug=True, host="0.0.0.0",port="3030")
--- openfast-outputs/pages/visualize_openfast.py
+++ openfast-outputs/pages/visualize_openfast.py
@@ -1,8 +1,8 @@
 '''This is the page for visualizing table and plots'''
 
 # Import Packages
-import dash
-from dash import Dash, Input, Output, callback, dcc, html, dash_table
+from dash import Dash, Input, Output, State, callback, dcc, html, register_page
+from dash.exceptions import PreventUpdate
 import copy
 import plotly.express as px
 import plotly.graph_objects as go
@@ -10,36 +10,59 @@ import plotly.tools as tls
 from plotly.subplots import make_subplots
 import dash_mantine_components as dmc
 import pandas as pd
+import logging
 
 
-layout = html.Div(id='body-div')
+register_page(
+    __name__,
+    name='OpenFAST',
+    top_nav=True,
+    path='/open_fast'
+)
+
+def layout():
+    layout = html.Div([
+        html.H1(
+            [
+                "OpenFAST Visualization"
+            ]
+        ),
+        html.Div(id='openfast-div')
+    ])
+    return layout
 
 
 @callback(
-        Output('body-div', 'children'),
-        Input('stored-data', 'data')
+        Output('openfast-div', 'children'),
+        Input('store', 'data')
 )
-def get_data(data):
-    df = pd.DataFrame(data)
-    return html.Div(
-        [
-            html.H5("Signal-y"),
-            dcc.Dropdown(
-                id="signaly", options=sorted(df.keys()), multi=True
-            ),
-            html.H5("Signal-x"),
-            dcc.Dropdown(
-                id="signalx", options=sorted(df.keys())
-            ),
-            html.Br(),
-            html.H5("Plot Options"),
-            dcc.RadioItems(
-                options=['single plot', 'multiple plot'], value='single plot', inline=True, id='plotOption'
-            ),
-            html.Br(),
-            dcc.Graph(id="line"),
-        ]
-    )
+def analyze(store):
+    if store == {}:        # Nothing happens if data is not uploaded yet..
+        logging.warning(f"Upload data first..")
+        raise PreventUpdate
+
+    else:
+        logging.warning(f"Analyze function..")
+        df = pd.DataFrame(store)
+        return html.Div(
+            [
+                html.H5("Signal-y"),
+                dcc.Dropdown(
+                    id="signaly", options=sorted(df.keys()), multi=True
+                ),
+                html.H5("Signal-x"),
+                dcc.Dropdown(
+                    id="signalx", options=sorted(df.keys())
+                ),
+                html.Br(),
+                html.H5("Plot Options"),
+                dcc.RadioItems(
+                    options=['single plot', 'multiple plot'], value='single plot', inline=True, id='plotOption'
+                ),
+                html.Br(),
+                dcc.Graph(id="line")
+            ]
+        )
 
 # Add controls to build the interaction
 @callback(
@@ -47,38 +70,39 @@ def get_data(data):
     Input(component_id="signalx", component_property="value"),
     [Input(component_id="signaly", component_property="value")],
     Input(component_id="plotOption", component_property="value"),
-    Input('stored-data', 'data')
+    Input('store', 'data')
 )
-def draw_graphs(signalx, signaly, plotOption, data):
+def draw_graphs(signalx, signaly, plotOption, store):
 
     # fig = tls.make_subplots(rows=1, cols=1, shared_xaxes=True, verical_spacing=0.009, horizontal_spacing=0.009)
     # fig = go.Figure()
 
-    df = pd.DataFrame(data)
+    if (signalx is None) or (signaly == []):        # Do not update the graph
+        raise PreventUpdate
 
-    if plotOption == 'single plot':
-        fig = make_subplots(rows = 1, cols = 1)
+    else:
+        df = pd.DataFrame(store)
+        if plotOption == 'single plot':
+            fig = make_subplots(rows = 1, cols = 1)
+            for col_idx, label in enumerate(signaly):
+                fig.append_trace(go.Scatter(
+                    x = df[signalx],
+                    y = df[label],
+                    mode = 'lines',
+                    name = label),
+                    row = 1,
+                    col = 1)
 
-        for col_idx, label in enumerate(signaly):
-            fig.append_trace(go.Scatter(
-                x = df[signalx],
-                y = df[label],
-                mode = 'lines',
-                name = label),
-                row = 1,
-                col = 1)
+        elif plotOption == 'multiple plot':
+            fig = make_subplots(rows = 1, cols = len(signaly))
 
-    elif plotOption == 'multiple plot':
-        fig = make_subplots(rows = 1, cols = len(signaly))
-
-        for col_idx, label in enumerate(signaly):
-            fig.append_trace(go.Scatter(
-                x = df[signalx],
-                y = df[label],
-                mode = 'lines',
-                name = label),
-                row = 1,
-                col = col_idx + 1)
-
-    
-    return fig
+            for col_idx, label in enumerate(signaly):
+                fig.append_trace(go.Scatter(
+                    x = df[signalx],
+                    y = df[label],
+                    mode = 'lines',
+                    name = label),
+                    row = 1,
+                    col = col_idx + 1)
+        
+        return fig
